70f10a485b64dc27eb0978bde18258366270f8bd
Zain-mir24 <zainnaeemk10@gmail.com>
2025-12-27 06:15:46 +0100

    added render yaml file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,12 +6,12 @@ var dotenvExpand = require('dotenv-expand');
 var myEnv = dotenv.config();
 const myvalue = dotenvExpand.expand(myEnv).parsed;
 
-const databaseUsername =myvalue.DATABASE_NAME;
-const databasePassword =myvalue.DATABASE_PASSWORD;
-const databaseName =myvalue.DATABASE_NAME;
-const databaseHost =myvalue.DATABASE_HOST;
+const databaseUsername = myvalue.DATABASE_USERNAME;
+const databasePassword = myvalue.DATABASE_PASSWORD;
+const databaseName = myvalue.DATABASE_NAME;
+const databaseHost = myvalue.DATABASE_HOST;
 const databasePort = myvalue.DATABASE_PORT;
-const databaseType=myvalue.DATABASE_TYPE;
+const databaseType = myvalue.DATABASE_TYPE;
 
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
