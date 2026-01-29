7db22629a601ad67037a0ee986047e178f4d10c6
Zain-mir24 <zainnaeemk10@gmail.com>
2026-01-29 16:42:49 +0100

    Ai module added

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { AiController } from './ai.controller';
-import { EventsService } from 'src/events/events.service';
+import { EventsService } from '../events/events.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from 'src/events/entities/event.entity';
-import { UsersModule } from 'src/users/users.module';
+import { Event } from '../events/entities/event.entity';
+import { UsersModule } from '../users/users.module';
 @Module({
   imports: [TypeOrmModule.forFeature([Event]),UsersModule],
   controllers: [AiController],
